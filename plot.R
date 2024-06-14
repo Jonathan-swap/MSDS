6e20ee1f408b64fbc07542b44c18f68b42d942c3
Jonathan Swap <js7jx@virginia.edu>
2024-06-14 16:28:22 -0400

    histogram plot

---
+++ plot.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+data1 <- read_xlsx("MSDS-Orientation-Computer-Survey.xlsx")
+
+
+
+
+f <- ggplot(data=data1, aes(x=`RAM (in GB)`)) + geom_histogram(fill="blue",alpha=.5) + 
+  ggtitle("Count of RAM (in GB) in the current and past MSDS cohorts")
+f
