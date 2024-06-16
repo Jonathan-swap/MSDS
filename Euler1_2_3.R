d25633cf1659f9f20d98acde11ffa5d4c3959f85
Jonathan Swap <js7jx@virginia.edu>
2024-06-16 10:26:15 -0400

    'second problem in R'

--- Euler1_2_3.R
+++ Euler1_2_3.R
@@ -1,12 +1,30 @@
-#question 1
-sumvar <- 0
-for (i in seq(0,999,by=1)){
-  if (i %% 3 == 0 | i %% 5 == 0){
-    sumvar <- i + sumvar
-    print(sumvar)
-    print(i)
+# #### question 1
+# sumvar <- 0
+# for (i in seq(0,999,by=1)){
+#   if (i %% 3 == 0 | i %% 5 == 0){
+#     sumvar <- i + sumvar
+#     print(sumvar)
+#     print(i)
+#   }
+# }
+# print(sumvar)
+
+
+#### question 2
+n <- 4000000
+num1 <- 0
+num2 <- 1
+next_num <- num2
+array <- c(0)
+
+while (next_num <= n) {
+  cat(next_num, " ")
+  num1 <- num2
+  num2 <- next_num
+  next_num <- num1 + num2
+  if (next_num %% 2 == 0) {
+    array <- c(array, next_num)
   }
 }
-print(sumvar)
 
-#question 2
+print(sum(array))
