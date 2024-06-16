c85c6b9f41b297890bf18f518c3bb1519b93ee63
Jonathan Swap <js7jx@virginia.edu>
2024-06-16 12:46:16 -0400

    class data histogram for Python

--- classdata.py
+++ classdata.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data1 = pd.read_csv("MSDS-Orientation-Computer-Survey.csv")
+data1 = pd.read_csv("/Users/jonathanswap/Desktop/MSDS summer semester/MSDS-Orientation-Computer-Survey(in).csv")
 
 sns.histplot(data1['RAM (in GB)'], bins=30, color='orange', alpha=0.5)
 plt.title("Count of RAM (in GB) in the current and past MSDS cohorts")
--- histograms.R
+++ histograms.R
@@ -1,7 +1,26 @@
 #binomial 
-
+n = 10
+p = .5
+samplesB = np.random.binomial(n, p, 100)
+plt.hist(samplesB, bins=12, alpha=.5,color="orange")
+plt.title('Binomial Distibution')
+plt.grid(False)
+plt.show()
 
 #normal 
+mu = 0
+sigma = 1
+samplesN = np.random.normal(mu, sigma, 100)
+plt.hist(samplesN, bins=12, alpha=.5, color="orange")
+plt.title('Normal Distribution')
+plt.grid(False)
+plt.show()
 
 
 #poisson
+lam = 3
+samplesP = np.random.poisson(lam,100)
+plt.hist(samplesP, bins=12, alpha=.5, color="orange")
+plt.title('Poisson Distribution')
+plt.grid(False)
+plt.show()
